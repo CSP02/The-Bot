d4aa4e9ef6998e4923bc87260b188921942480c6
chandra sekhar pilla <65494930+Chandra-sekhar-pilla@users.noreply.github.com>
2021-01-28 07:45:53 +0530

    Update main.js

--- main.js
+++ main.js
@@ -61,4 +61,4 @@ client.on('message', message => {
         message.channel.send('No such command exist till now');
     }
 });
-client.login(process.env.Token);
+client.login('ODAzNDkwMTM3MDIyNzkxNjgw.YA-igA.Qs-EMeRegk57Q5ldq2QkWIMYa7Q');
