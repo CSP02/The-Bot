6767001c1a415f6af1b1498c3ad9a6fd4e7a5819
chandra sekhar pilla <65494930+Chandra-sekhar-pilla@users.noreply.github.com>
2021-02-06 18:03:58 +0530

    Create userinfo.js

---
+++ commands/userinfo.js
@@ -0,0 +1,47 @@
+module.exports = {
+    name: 'userinfo',
+    description: "This is ping command",
+    execute(client, message, args, Discord) {
+      let mentioned = message.mentions.users.first();
+      if(!mentioned){
+        const embedMsg = new Discord.MessageEmbed()
+            .setColor('#66ff66')
+            .setTitle('User Info:')
+            .setThumbnail(`${message.author.displayAvatarURL()}`)
+
+            .addFields(
+              {name: 'User Tag:', value: `${message.author.tag}`},
+                { name: 'User ID:', value: `${message.author.id}`},
+             
+             
+             { name: 'User Name:', value: `${message.author.username}` },
+             
+             { name: 'User Created:', value: `${message.author.createdAt}`}
+
+            )
+            message.channel.send(embedMsg);
+      }else{
+        const embedMsg = new Discord.MessageEmbed()
+
+            .setColor('#66ff66')
+
+            .setTitle('User Info:')
+  
+            .setThumbnail( `${mentioned.displayAvatarURL()}`)
+
+            .addFields(
+              {name: 'User Tag:' , value: `${mentioned.tag}`},
+                { name: 'User ID:', value: `${mentioned.id}`},
+             
+             
+             { name: 'User Name:', value: `${mentioned.username}` },
+             
+             { name: 'User Created:', value: `${mentioned.createdAt}`}
+            )
+            .setFooter('make sure to check the rules before messaging');
+            
+            message.channel.send(embedMsg);
+      }
+
+    }
+}
