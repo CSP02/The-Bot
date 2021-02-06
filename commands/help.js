740c6f7a2d3ddde26a05146b1fc763eb1418276c
chandra sekhar pilla <65494930+Chandra-sekhar-pilla@users.noreply.github.com>
2021-02-06 17:54:09 +0530

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -1,15 +1,43 @@
 module.exports = {
+
     name: 'help',
+
     description: "This is ping command",
+
     execute(client, message, args, Discord) {
+
         const embedMsg = new Discord.MessageEmbed()
+
             .setColor('#00ff00')
-            .setTitle('Help')
+
+            .setTitle('__Help__:')
+
             .addFields(
-                { name: 'Roles:', value: 'member' },
-                { name: 'Ping:', value: 'Pong' },
+
+                { name: 'Roles(not the command):', value: 'cg artist\nGamers\nGame devs' },
+
+                {name: 'userinfo', value: 'Displays the userinfo of the messaged member'},
+
+                {name: 'serverinfo', value: 'Displays the brief info of the server'},
+
+                
+
+                { name: 'Ping:', value: 'Shows the hearth beat of the bot.' },
+
+                { name: 'kick:', value: 'kicks the mentioned member' },
+
+                { name: 'mute:', value: 'mutes the mentioned member for specified amount of time' },
+
+                { name: 'ban:', value: 'bans the mentioned member.' },
+
+                { name: 'unmute', value: 'unmutes the mentioned member'}
+
                  )
-            .setFooter('make sure to check the rules before messaging');
+
+            .setFooter('Make sure to check the rules before messaging.\nAlso note that mute, ban, unmute, kick commands were only used by the moderator or the admins.');
+
         message.channel.send(embedMsg);
+
     }
+
 }
