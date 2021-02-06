f02c8956a8563d0e0f1716d59a21256e140e32d7
chandra sekhar pilla <65494930+Chandra-sekhar-pilla@users.noreply.github.com>
2021-02-06 19:42:21 +0530

    Added github command
    Added github command which will send a link of this repository.

---
+++ commands/github.js
@@ -0,0 +1,10 @@
+module.exports = {
+
+    name: 'github',
+
+    description: "Displays the link for the bot's repository.",
+    execute(client, message, args, Discord){
+        
+        message.send.channel('https://github.com/Chandra-sekhar-pilla/The-Bot');
+    }
+}
