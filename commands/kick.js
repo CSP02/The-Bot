23646b9a2407d1b1fca14a44a332048fa35ec5e2
chandra sekhar pilla <65494930+Chandra-sekhar-pilla@users.noreply.github.com>
2021-02-06 17:55:54 +0530

    Update kick.js

--- commands/kick.js
+++ commands/kick.js
@@ -1,31 +1,56 @@
+
 module.exports = {
+
     name: 'kick',
+
     description: "kick command",
+
     execute(client, message, args, Discord) {
-        if (message.member.roles.cache.some(r => r.name === "king")) {
+
+        if (message.member.roles.cache.some(r => r.name === "admin" || message.member.roles.cache.some(r => r.name === "mod"))) {
+
             const target = message.mentions.users.first();
-            message.channel.send(target);
+
             if (target) {
+
                 const memberTarget = message.guild.members.cache.get(target.id);
+
                 memberTarget.kick();
 
                 const embedMsg = new Discord.MessageEmbed()
+
                     .setColor('#ff0000')
+
                     .setTitle('Kicked:')
+
                     .setDescription(`<@${memberTarget.user.id}> has been kicked`)
+
                     .addFields(
-                        { name: 'Reason:', value: `${args[1]}` }
+
+                        { name: 'Reason:', value: `${args.slice(1).join(" ")}` }
 
                     )
+
                 message.channel.send(embedMsg);
+
                 //message.channel.send(`<@${memberTarget.user.id}> has been muted`);
+
             }
+
             else {
+
                 message.channel.send('cant find that member');
+
             }
+
         }
+
         else {
+
             message.reply('you have no permission');
+
         }
+
     }
+
 }
