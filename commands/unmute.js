2ad6cf9eb382d8fcd2d365a22b6049549f668b23
chandra sekhar pilla <65494930+Chandra-sekhar-pilla@users.noreply.github.com>
2021-02-06 18:03:14 +0530

    Update unmute.js

--- commands/unmute.js
+++ commands/unmute.js
@@ -1,18 +1,18 @@
 module.exports = {
     name: 'unmute',
-    description: "This is ping command",
-    execute(client, message, args){
-        if(message.member.roles.cache.some(r => r.name === "king")){
+    description: "This command will unmute the mentiomed member.",
+    execute(client, message, args, Discord){
+        if(message.member.roles.cache.some(r => r.name === "admin") || message.member.roles.cache.some(r => r.name === "mod" )){
             const target = message.mentions.users.first();
         if(target){
-            let mainRole = message.guild.roles.cache.find(role => role.name === 'member');
+            let mainRole = message.guild.roles.cache.find(role => role.name === 'shrimp');
             let mutedRole = message.guild.roles.cache.find(role => role.name === 'Muted');
 
             memberTarget = message.guild.members.cache.get(target.id);
 
             memberTarget.roles.remove(mutedRole.id);
             memberTarget.roles.add(mainRole.id);
-            message.channel.send(`<@${memberTarget.user.id}> has been unmuted`);
+message.channel.send(`<@${memberTarget.user.id}> has been unmuted`);
         }
         else{
             message.channel.send('cant find that member');
