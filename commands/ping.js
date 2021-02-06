49f6c999c258693fcbe3d64b84acae0b88600920
chandra sekhar pilla <65494930+Chandra-sekhar-pilla@users.noreply.github.com>
2021-02-06 17:50:15 +0530

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -1,12 +1,15 @@
 module.exports = {
+
     name: 'ping',
+
     description: "This is ping command",
-    execute(client, message, args){
-        if(message.guild.roles.cache.find(role => role.name === 'king')){
-            message.channel.send('pong!');
-        } 
-        else{
-            message.channel.send('you have no permission to use this command');
-        } 
+
+    execute(client, message, args, Discord){
+
+        
+
+            message.channel.send(`Present heartbeat of the bot is ${client.ws.ping}ms.`);
+
     }
+
 }
