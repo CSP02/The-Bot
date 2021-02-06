1048370e5dcbff7c61f1a598b3a6e5b2d137fc2e
chandra sekhar pilla <65494930+Chandra-sekhar-pilla@users.noreply.github.com>
2021-02-06 17:52:42 +0530

    Update ban.js

--- commands/ban.js
+++ commands/ban.js
@@ -1,36 +1,4 @@
 module.exports = {
-    name: 'ban',
-    description: "This is ping command",
-    execute(client, message, args, Discord) {
-        if (message.member.roles.cache.some(r => r.name === "king")) {
-            const target = message.mentions.users.first();
-            if (target) {
-                let mainRole = message.guild.roles.cache.find(role => role.name === 'member');
-                let mutedRole = message.guild.roles.cache.find(role => role.name === 'Muted');
-
-                memberTarget = message.guild.members.cache.get(target.id);
-
-                memberTarget.roles.remove(mainRole.id);
-                memberTarget.roles.add(mutedRole.id);
-                const embedMsg = new Discord.MessageEmbed()
-                    .setColor('#ff0000')
-                    .setTitle('Banned:')
-                    .setDescription(`<@${memberTarget.user.id}> has been banned`)
-                    .addFields(
-                        { name: 'Reason:', value: `${args[1]}` }
-
-                    )
-                message.channel.send(embedMsg);;
-            }
-            else {
-                message.channel.send('cant find that member');
-            }
-        }
-        else {
-            message.reply('you have no permission');
-        }
-    }
-}module.exports = {
 
     name: 'ban',
 
