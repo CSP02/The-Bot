eee3a9062d563ba41d3f6100d24af710d19824d7
chandra sekhar pilla <65494930+Chandra-sekhar-pilla@users.noreply.github.com>
2021-02-06 18:01:22 +0530

    Update role.js

--- commands/role.js
+++ commands/role.js
@@ -3,7 +3,7 @@ module.exports = {
 	description: 'This is ping command',
 	execute(client, message, args, Discord) {
 		if (args[0]) {
-				 if((args[0] ===  'Moderator' || args[0] ===  'king') && message.member.roles.cache.some(r => r.name === 'king')){				
+				 if((args[0] ===  'mod' || args[0] ===  'admin') && message.member.roles.cache.some(r => r.name === 'admin')){				
 				let myRole = message.guild.roles.cache.find(role => role.name === args[0]);
 			
 				message.member.roles.add(myRole).catch(console.error);
@@ -18,31 +18,10 @@ module.exports = {
             message.channel.send('No such role is found. Check the spelling and roles are also case sensitive.');
           }
         }else{
-          message.reply(`You have no permission to add the ${args[0]} role\nAsk any of the king to assign you the role.`);
+          message.reply(`You have no permission to add the ${args[0]} role\nAsk any of the admin to assign you the role.`);
         }
 		}else{
 		  message.reply('Please specify the role you want.');
 		}
 	}
 }
-
-/*if (message.member.roles.cache.some(r => r.name === 'king')) {
-					message.reply('you already have king role');
-				} else {
-					message.reply('You have no permission to get king role.');
-				}
-} else*/
-
-/*module.exports = {
-    name: 'role',
-    description: "This is ping command",
-    execute(client, message, args){
-        if(message.member.roles.cache.some(r => r.name === "king")){
-            message.channel.send('you already has the king role');
-        } 
-        else{
-            message.member.roles.add('803500474937376778').catch(console.error);
-            message.channel.send('Enjoy the king role');
-        } 
-    }
-}*/
