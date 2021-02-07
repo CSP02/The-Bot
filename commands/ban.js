104ed95a0f21b5d56f92164bf17adc74272e73cd
chandra sekhar pilla <65494930+Chandra-sekhar-pilla@users.noreply.github.com>
2021-02-07 10:59:05 +0530

    Update ban.js

--- commands/ban.js
+++ commands/ban.js
@@ -1,57 +1,30 @@
 module.exports = {
-
     name: 'ban',
-
     description: "ban command",
-
     execute(client, message, args, Discord) {
-
-        if (message.member.roles.cache.some(r => r.name === "admin")) {
-
+        if (message.member.hasPermission('BAN_MEMBERS')) {
             const target = message.mentions.users.first();
-
             message.channel.send(target);
-
             if (target) {
-
                 const memberTarget = message.guild.members.cache.get(target.id);
-
                 memberTarget.ban();
 
                 const embedMsg = new Discord.MessageEmbed()
-
                     .setColor('#ff0000')
-
                     .setTitle('Banned:')
-
                     .setDescription(`<@${memberTarget.user.id}> has been banned`)
-
                     .addFields(
-
                         { name: 'Reason:', value: `${args[1]}` }
 
                     )
-
                 message.channel.send(embedMsg);
-
-                //message.channel.send(`<@${memberTarget.user.id}> has been muted`);
-
             }
-
             else {
-
                 message.channel.send('cant find that member');
-
             }
-
         }
-
         else {
-
             message.reply('you have no permission');
-
         }
-
     }
-
 }
