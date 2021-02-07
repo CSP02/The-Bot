055a47da20bdeedb98471a572d70e24ade9fcc9b
chandu-programmer <65494930+chandu-programmer@users.noreply.github.com>
2021-02-07 12:08:44 +0530

    updated

--- handlers/event_handler.js
+++ handlers/event_handler.js
@@ -1,31 +1,32 @@
 const fs = require('fs');
 
 module.exports = (client, Discord) => {
-	const load_dir = dir => {
-		const event_files = fs
-			.readdirSync(`./events/${dir}`)
-			.filter(file => file.endsWith('.js'));
+    const load_dir = dir => {
+        const event_files = fs
+            .readdirSync(`./events/${dir}`)
+            .filter(file => file.endsWith('.js'));
 
-		for (const file of event_files) {
-			const event = require(`../events/${dir}/${file}`);
-			const event_name = file.split('.')[0];
-			client.on(event_name, event.bind(null, Discord, client));
+        for (const file of event_files) {
+            const event = require(`../events/${dir}/${file}`);
+            const event_name = file.split('.')[0];
+            client.on(event_name, event.bind(null, Discord, client));
 
-            client.on('ready', ()=> {
-                client.user.setActivity("Moderation", {
-                    type: "PLAYING"
+
+            client.on('ready', () => {
+                client.user.setActivity("Helping\n(!help)", {
+                    type: "PLAYING",
                 })
             });
 
-			client.on('guildMemberAdd', guildMember => {
-				let welcomeRole = guildMember.guild.roles.cache.find(
-					role => role.name === 'member'
-				);
+            client.on('guildMemberAdd', guildMember => {
+                let welcomeRole = guildMember.guild.roles.cache.find(
+                    role => role.name === 'member'
+                );
 
-				guildMember.roles.add(welcomeRole);
-			})
-		}
-	};
+                guildMember.roles.add(welcomeRole);
+            })
+        }
+    };
 
-	['client', 'guild'].forEach(e => load_dir(e));
+    ['client', 'guild'].forEach(e => load_dir(e));
 }
