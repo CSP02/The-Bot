dba91fd2c2fe950f7319d8d593d1caa63a0d3a71
chandu-programmer <65494930+chandu-programmer@users.noreply.github.com>
2021-02-23 19:37:43 +0530

    bug cleared

--- events/guild/guildMemberAdd.js
+++ events/guild/guildMemberAdd.js
@@ -1,25 +1,18 @@
-module.exports = (Discord, client) => {
-    name : 'guildMemberAdd',
+module.exports = (Discord, client, guildMember) => {
 
+    const sLogsChannel = client.channels.cache.get('811997907473268788')
+    const taLogsChannel = client.channels.cache.get('810141713205035019')
 
-        guildMember => {
-            const sLogsChannel = client.channels.cache.get('811997907473268788')
-            const taLogsChannel = client.channels.cache.get('810141713205035019')
+    const server = guildMember.guild
 
-            const server = message.guild
+    let welcomeRole = guildMember.guild.roles.cache.find(
+        role => role.name === 'member'
+    );
 
-            let welcomeRole = guildMember.guild.roles.cache.find(
-                role => role.name === 'member'
-            );
-
-            if (server.id === '801451603860258861') {
-                sLogsChannel.send(`hurray! ${guildMember} has joined.`)
-            } else if (server.id === '795133444610457640') {
-                taLogsChannel.send(`hurray! ${guildMember} has joined.`)
-            }
-            guildMember.roles.add(welcomeRole);
-        }
-
-
-    client.channels.cache.get('810141713205035019').send('member added')
+    if (server.id === '801451603860258861') {
+        sLogsChannel.send(`hurray! ${guildMember} has joined.`)
+    } else if (server.id === '795133444610457640') {
+        taLogsChannel.send(`hurray! ${guildMember} has joined.`)
+    }
+    guildMember.roles.add(welcomeRole);
 }
