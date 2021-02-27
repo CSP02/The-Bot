78530d372a882216f9006bb25adec9d5ce68df9a
chandu-programmer <65494930+chandu-programmer@users.noreply.github.com>
2021-02-27 19:05:11 +0530

    Update clear.js
    now the bot can delete the messages by the mentioned user of specified length.

--- commands/clear.js
+++ commands/clear.js
@@ -1,20 +1,35 @@
 module.exports = {
     name: 'clear',
-    description: "This is ping command",
+    description: 'clear command',
     async execute(client, message, args, Discord) {
-
         if (message.member.hasPermission('MANAGE_MESSAGES')) {
-            
-            if (!args[0]) return message.reply("please enter the number of messages you want to clear");
-            
-            if (isNaN(args[0])) return message.reply('please enter a real number');
 
-            if (args[0] > 100) return message.reply('you cannot');
-            if (args[0] < 1) return message.reply('you cant time travel');
+            const target = message.mentions.users.first();
+            const messages = message.channel.messages.fetch({ limit: args[1] })
+            
+            if (target) {
+                if (!args[1]) return message.reply("please enter the number of messages you want to clear");
 
-            await message.channel.messages.fetch({ limit: args[0] }).then(messages => {
-                message.channel.bulkDelete(messages);
-            });
+                if (isNaN(args[1])) return message.reply('please enter a real number');
+
+                if (args[1] > 100) return message.reply('you cannot');
+                if (args[1] < 1) return message.reply('you cant time travel');
+
+                const userMessage = (await messages).filter(m => m.author.id === message.author.id);
+                await message.channel.bulkDelete(userMessage)
+
+            } else {
+                if (!args[0]) return message.reply("please enter the number of messages you want to clear");
+
+                if (isNaN(args[0])) return message.reply('please enter a real number');
+
+                if (args[0] > 100) return message.reply('you cannot');
+                if (args[0] < 1) return message.reply('you cant time travel');
+
+                await message.channel.messages.fetch({ limit: args[0] }).then(messages => {
+                    message.channel.bulkDelete(messages);
+                });
+            }
         }
         else {
             message.reply('you have no permission to use this command');
