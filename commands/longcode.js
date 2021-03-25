9c2659d9f8a27459195a6b893dcd9a01af41be8a
chandra sekhar pilla <65494930+Chandra-sekhar-pilla@users.noreply.github.com>
2021-03-25 17:41:49 +0530

    Create longcode.js

---
+++ commands/longcode.js
@@ -0,0 +1,11 @@
+module.exports = {
+
+    name: 'longcode',
+
+    description: "sends a link of pastemyst",
+    aliases: ['Longcode', 'long','lc'],
+    execute(client, message, args, Discord) {
+
+        message.channel.send('Want to send a long code? use pastemyst here is the link\nhttps://paste.myst.rs');
+    }
+}
