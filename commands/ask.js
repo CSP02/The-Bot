d700eb58d5ca863a19845605ae651225919768bf
Dead Shadøw <76168914+Dead-Shadow-12119@users.noreply.github.com>
2021-04-12 19:41:22 +0530

    Simple magic8ball command.

---
+++ commands/ask.js
@@ -0,0 +1,25 @@
+module.exports = {
+    name: 'ask',
+    description: "Ask a random question and bot will answer it. ",
+    aliases: ['asks', 'question'],
+    execute(client ,message, args , Discord){
+        
+        let question = args[0];
+        if (!question) return message.channel.send("Please enter the question ! ");
+        
+        const replies = ["Yes","No","Maybe","Never","Definitely !","Ask again later","Most likely","Cannot predict now."];
+        const answer = Math.floor(Math.random() * replies.length);
+        const questions = args.join(" ");
+        
+        const embed = new Discord.MessageEmbed()
+        .setColor('#e08aff')
+        .setTitle('The magic 8 ball says:') 
+        .addFields(
+         { name: 'Question:', value: questions},
+         { name: 'Answer:', value: replies[answer]}
+        )
+        .setFooter(message.author.username, message.author.displayAvatarURL())
+        .setTimestamp()
+        message.channel.send(embed);
+    }
+}
