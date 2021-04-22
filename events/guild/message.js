e01f1bfc5342dff2d89275eae003bd20c7951390
chandra sekhar pilla <65494930+Chandra-sekhar-pilla@users.noreply.github.com>
2021-04-22 13:58:05 +0530

    Update message.js

--- events/guild/message.js
+++ events/guild/message.js
@@ -1,34 +1,41 @@
 module.exports = (Discord, client, message) => {
-    const prefix = 'prefix';
+	const prefix = 'prefix';
 
-    const words = [
-        //all the cuss words here with a comma
+	const words = [
+		//All the words goes here
+	];
 
-    ]
+	if (!message.content.startsWith(prefix) || message.author.bot) {
 
-    if (!message.content.startsWith(prefix) || message.author.bot || message.channel.type === 'dm') {
-        for (const curseWords of words) {
-            if (message.content.toLowerCase().includes(curseWords)) {
-                curseMsg = message.fetch(message.id)
-                if (curseMsg) {
-                    message.delete(curseMsg)
-                    message.reply("Message deleted,\nPlease don't use the those words unless you want to get **muted** or **banned**");
-                }
-                else {
-                    return;
-                }
-            } else {
-                continue;
-            }
-        }
-        return
-    }
+			for (const curseWords of words) {
+				if (message.content.toLowerCase().includes(curseWords)) {
+					curseMsg = message.fetch(message.id);
+					if (curseMsg) {
+						message.delete(curseMsg);
+						message.reply(
+							"Message deleted,\nPlease don't use the those words unless you want to get **muted** or **banned**"
+						);
+					} else {
+						return;
+					}
+				} else {
+					continue;
+				}
+			}
+			return;
+		}
+	
+	if(message.channel.type === 'dm') return message.reply("Sorry! Iam not for dms.")
+else{
+	const args = message.content.slice(prefix.length).split(/ +/);
+	const cmd = args.shift().toLowerCase();
 
-    const args = message.content.slice(prefix.length).split(/ +/);
-    const cmd = args.shift().toLowerCase();
+	const command =
+		client.command.get(cmd) ||
+		client.command.find(a => a.aliases && a.aliases.includes(cmd));
 
-    const command = client.command.get(cmd) || client.command.find(a => a.aliases && a.aliases.includes(cmd));
-
-    if (command) command.execute(client, message, args, Discord);
-    else message.channel.send('No such command exists so far.');
+	if (command) 
+	command.execute(client, message, args, Discord);
+	else message.channel.send('No such command exists so far.');
+}
 }
