11796dc5ae50ab2a6448139007b8d349547bd6dd
chandu-programmer <65494930+Chandra-sekhar-pilla@users.noreply.github.com>
2021-05-16 20:09:37 +0530

    added remove role and infractions commands, updated userinfo command

--- commands/Moderation/warns.js
+++ commands/Moderation/warns.js
@@ -1,36 +1,58 @@
-const mongo = require("../mongo")
-const schema = require("../schema")
+const mongo = require("../../mongo")
+const schema = require("../../schema")
 
 module.exports = {
     name: 'warns',
     description: 'replies with the previous warns done by the mentioned user',
-    aliases: ['list-warns', 'lstwarns', 'lw'],
+    aliases: ['list-warns', 'lstwrns', 'lw'],
     async execute(client, message, args, Discord) {
-        const target = message.mentions.users.first()
-        if (!target) return message.reply("Please mention the user")
+        let infrCount = parseInt('0', 10);
+        let target = message.mentions.users.first() || message.guild.members.cache.get(args[0]).user
 
-        const guildId = message.guild.id
-        const userId = message.member.id
+        if (!target && !args[0]) return message.reply("please mention the user")
+        if (target) {
+            Warns(target)
+        } else if (args[0] && !isNaN(args[0])) {
+            client.users.fetch(args[0]).then(ment => {
+                Warns(ment)
+            })
+        } else {
+            message.reply('cant find member')
+        }
 
-        await mongo().then(async mongoose => {
-            try {
-                const results = await schema.findOne({
-                    guildId,
-                    userId
-                })
 
-                let reply = `Warnings of user <@${target.id}>:\n\n`
+        async function Warns(target) {
+            const guildId = message.guild.id
+            const userId = message.member.id
+            let reply = ' '
 
-                for (const warning of results.warnings) {
-                    const { author, user, timestamp, reason } = warning
-                    if (user == target.tag) {
-                        reply += `${author} warned ${user} on ${new Date(timestamp).toLocaleDateString()} for ${reason}.\n\n`
-                        message.channel.send(reply)
+            await mongo().then(async mongoose => {
+                try {
+                    const results = await schema.findOne({
+                        guildId
+                    })
+
+                    for (const warning of results.warnings) {
+                        const { author, user, timestamp, reason, infrID } = warning
+                        if (user == target.tag) {
+                            infrCount++;
+                            reply += `${author} warned ${user} on ${new Date(timestamp).toLocaleDateString()} for ${reason}.\n\nInfraction ID: ${infrID}\n\n`
+                        }
+                        console.log(`${infrCount}`)
                     }
+                } finally {
+                    mongoose.connection.close()
                 }
-            } finally {
-                mongoose.connection.close()
-            }
-        })
+                const embdmsg = new Discord.MessageEmbed()
+                    .setTitle('Warns')
+                    .setColor('#ff0000')
+                    .setFooter(`Warns of the mentioned user and their infraction IDs respectively.`)
+                    .addFields(
+                        { name: 'Warnings of user:', value: `${target}` },
+                        { name: 'Warnings:', value: `${reply}` },
+                    )
+                message.channel.send(embdmsg)
+            })
+        }
     }
 }
---
+++ commands/Utility/removerole.js
@@ -0,0 +1,29 @@
+module.exports = {
+    name: 'removerole',
+    aliases: ['remove', 'rr', 'removerole'],
+    description: 'removes a specified role',
+    execute(client, message, args, Discord) {
+        const cacheTeam = message.member.roles.cache
+        if (args[0]) {
+            if (args[0] != 'mod' && args[0] != 'admin' && args[0] != 'Muted') {
+                let myRole = message.member.roles.cache.find(role => role.name === args.slice(0).join(" ").toLowerCase());
+                if (myRole) {
+                    message.member.roles.remove(myRole).catch(console.error);
+                    const embedMsg = new Discord.MessageEmbed()
+                        .setColor('#C155FF')
+                        .setTitle('__Role__:')
+                        .addFields({ name: 'Role successfully removed', value: `${myRole}` },)
+                    message.channel.send(embedMsg);
+                }
+                else if (message.guild.roles.cache.find(role => role.name === `Team ${args.slice(0).join(" ").toLowerCase()}`)) {
+                    message.channel.send("No such team found in this server.")
+                }
+                else {
+                    message.channel.send('Seems like you did not have that role.');
+                }
+            }
+        } else {
+            message.channel.send("Spedify a role to remove.")
+        }
+    }
+}
