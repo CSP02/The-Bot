a1e491222fe9f221624f89df330a1c0ae0f4cd1e
Dead Shadøw <76168914+Dead-Shadow-12119@users.noreply.github.com>
2021-05-23 16:29:48 +0530

    Update javascript.js

--- commands/Soft Moderation/javascript.js
+++ commands/Soft Moderation/javascript.js
@@ -5,8 +5,36 @@ module.exports = {
     execute(client ,message, args , Discord){
        
         const embed = new Discord.MessageEmbed()
-        .setColor('#00FAF3')
-        .setDescription("__**Documentation:**__ \n•MDN Javascript: https://developer.mozilla.org/en-US/docs/Web/JavaScript \n•W3School (Javascript): https://www.w3schools.com/js/ \n•jQuery Documentation: http://contribute.jquery.org/documentation/ \n•NodeJS: https://nodejs.org/en/docs/ \n \n__**Tutorials:**__ \n•Eloquent Javascript: http://eloquentjavascript.net/ \n•You Don't Know JS: https://github.com/getify/You-Dont-Know-JS \n•MDN Tutorial: https://developer.mozilla.org/en-US/docs/Learn/JavaScript/First_steps \n•Modern Javascript: https://javascript.info/ \n \n__**Interesting:**__ \n•You might not need jQuery: http://youmightnotneedjquery.com/ \n•CodingTrain (tutorials with examples using p5.js): https://www.youtube.com/user/shiffman \n \nThis list does not contain every resource out there because the internet is huge and listing everything down is impossible.")
+        .setColor('#DDFF00')
+        .setThumbnail("https://i.ibb.co/5sy5y5H/javascript-logo.png")
+        .addFields(      
+              { name: '__**Documentation:**__', value:'\u200b'},
+              
+              { name: '•MDN Javascript: ', value: 'https://developer.mozilla.org/en-US/docs/Web/JavaScript '},
+              
+              { name: '•W3School (Javascript):  ', value: 'https://www.w3schools.com/js/ '},
+              
+              { name: '•jQuery Documentation: ', value: 'http://contribute.jquery.org/documentation/  '},
+              
+              { name: '•NodeJS: ', value: 'https://nodejs.org/en/docs/ '},
+              
+              { name: '__**Tutorials:**__ ', value : '\u200b'},
+              
+              { name: '•Eloquent Javascript: ', value: ' http://eloquentjavascript.net/'},
+              
+              { name: "•You Don't Know JS: ", value: 'https://github.com/getify/You-Dont-Know-JS '},
+              
+              { name: '•Modern Javascript: ', value: 'https://javascript.info/ '},
+              
+              { name: '__**Interesting:**__', value:'\u200b'},
+              
+              { name: '•You might not need jQuery: ', value: 'http://youmightnotneedjquery.com/ '},
+              
+              { name: '•CodingTrain (tutorials with examples using p5.js): ', value: ' https://www.youtube.com/user/shiffman'}
+            
+              )
+              .setFooter("This list does not contain every resource out there because the internet is huge and listing everything down is impossible.")
+        
         message.channel.send(embed);
         
     }
